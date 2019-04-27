f10a8dd2377ed38a8dd30dd75e7aa6ee64bd8e4d
Jackie Santana <jackiebrown.nyc@gmail.comgit config --global user.email jackiebrown.nyc@gmail.com>
2019-04-26 21:31:57 -0500

    added color in about me

--- js/index.js
+++ js/index.js
@@ -37,10 +37,6 @@ fColor1()
 lColor1()
 }
 
-function myStopFunction() {
-    clearTimeout(nameColor);
-  }
-  myStopFunction()
 
 setInterval(nameColor, 1000)
 
@@ -60,3 +56,105 @@ function stopColor(){
 
 
 setInterval(stopColor, 6000)
+
+//change About me Color
+
+const a = document.querySelector('#a');
+const b = document.querySelector('#b');
+const o = document.querySelector('#o');
+const u = document.querySelector('#u');
+const t = document.querySelector('#t');
+
+const m = document.querySelector('#m');
+const e = document.querySelector('#e');
+
+const colorSwitch2 = 'on';
+
+var setFunc = function nameColor2(){
+    if(colorSwitch2 === 'on'){
+    function a1(){
+        
+            setTimeout(function (){ a.style.color = 'hotpink'; }, 1000)
+
+    }
+    
+    
+    
+    function b2(){
+        setTimeout(function (){ b.style.color = 'hotpink'; }, 2000)
+    }
+
+    function o3(){
+        setTimeout(function (){ o.style.color = 'hotpink'; }, 3000)
+    }
+
+    function u4(){
+        setTimeout(function (){ u.style.color = 'hotpink'; }, 4000)
+    }
+
+    function t5(){
+        setTimeout(function (){ t.style.color = 'hotpink'; }, 5000)
+    }
+
+    function m6(){
+        setTimeout(function (){ m.style.color = 'hotpink'; }, 6000)
+    }
+
+    function e7(){
+        setTimeout(function (){ e.style.color = 'hotpink'; }, 7000)
+    }
+}
+    a1()
+    b2()
+    o3()
+    u4()
+    t5()
+    m6()
+    e7()
+    
+}
+
+setInterval(setFunc, 1000)
+
+function nameColor3(){
+    function a1(){
+        if(colorSwitch2 === 'on'){
+            setTimeout(function (){ a.style.color = 'cyan'; }, 1000)
+        }
+    }
+    
+    
+    function b2(){
+        setTimeout(function (){ b.style.color = 'cyan'; }, 2000)
+    }
+
+    function o3(){
+        setTimeout(function (){ o.style.color = 'cyan'; }, 3000)
+    }
+
+    function u4(){
+        setTimeout(function (){ u.style.color = 'cyan'; }, 4000)
+    }
+
+    function t5(){
+        setTimeout(function (){ t.style.color = 'cyan'; }, 5000)
+    }
+
+    function m6(){
+        setTimeout(function (){ m.style.color = 'cyan'; }, 6000)
+    }
+
+    function e7(){
+        setTimeout(function (){ e.style.color = 'cyan'; }, 7000)
+    }
+    a1()
+    b2()
+    o3()
+    u4()
+    t5()
+    m6()
+    e7()
+    
+}
+
+
