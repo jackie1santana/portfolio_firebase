8e6447a992c372658a325cea56b52d2eb26a87ce
Jackie Santana <jackiebrown.nyc@gmail.comgit config --global user.email jackiebrown.nyc@gmail.com>
2019-04-26 20:32:35 -0500

    add javascript to name/color

--- js/index.js
+++ js/index.js
@@ -16,3 +16,47 @@ function myFunction() {
     }
 }
 
+//Transitioning the color of the name 
+const firstColor = document.querySelector('.first-name');
+const lastColor = document.querySelector('.last-name');
+const colorSwitch = 'on';
+
+function nameColor(){
+function fColor1(){
+    if(colorSwitch === 'on'){
+        setTimeout(function (){ firstColor.style.color = 'hotpink'; }, 1000)
+    }
+    
+}
+
+function lColor1(){
+    setTimeout(function (){ lastColor.style.color = 'cyan'; }, 3000)
+}
+
+fColor1()
+lColor1()
+}
+
+function myStopFunction() {
+    clearTimeout(nameColor);
+  }
+  myStopFunction()
+
+setInterval(nameColor, 1000)
+
+
+function stopColor(){
+    function fColor2(){
+        setTimeout(function (){ firstColor.style.color = 'white'; }, 1000)
+    }
+    
+    function lColor2(){
+        setTimeout(function (){ lastColor.style.color = 'white'; }, 3000)
+    }
+    
+    fColor2()
+    lColor2()
+}
+
+
+setInterval(stopColor, 6000)
